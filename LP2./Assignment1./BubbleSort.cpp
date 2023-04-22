9be704d4e0641174e31bf0edd0c1ec84aabbeb1e
abhi0627 <87473810+abhi0627@users.noreply.github.com>
2023-04-22 10:22:48 +0530

    Update BubbleSort.cpp

--- LP2./Assignment1./BubbleSort.cpp
+++ LP2./Assignment1./BubbleSort.cpp
@@ -1,1 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubbleSort(vector<int>& arr, int n)
+{   
+    for(int i = 1; i<n; i++) //for round 1 to n-1
+    {
+        bool swapped = false;
+        
+        for(int j =0; j<n-i; j++) //process element till n-i th index
+        {
+            
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+                swapped = true;
+            }
+            
+        }
+        
+        if(swapped == false) {
+            //already sorted
+            break;
+        }
+           
+    }
+}
 
+void solve(){
+   int n;
+   cout<<"enter the no of elements in your array : ";
+   cin>>n;
+
+   cout<<"enter the elements one by one : ";
+   vector<int>arr(n);
+
+    bubbleSort(arr,n);
+
+
+
+};
